49b14114c3094f45f68b152bc434cffed11289cc
Michael Maldonado <MichaelFrankMaldonado@gmail.com>
2022-12-27 21:13:27 -0700

    Exercise 1

---
+++ Classes/Cars.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Classes
+{
+    public class Car
+    {
+        public string Make { get; set; }   //Property
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -2,9 +2,19 @@
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car();   //Instance
+
+            car1.Make = "GMC";
+            car1.Model = "1500";
+            car1.Year = 2014;
+
+            Console.WriteLine(car1.Make);
+            Console.WriteLine(car1.Model);
+            Console.WriteLine(car1.Year);
         }
+    
     }
 }
